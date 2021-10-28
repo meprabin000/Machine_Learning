b0f42d2c9aebb344668b804a7209ec4f81f6dcdc
iamprabin <meprabin000@gmail.com>
2021-10-28 16:51:36 -0500

    Updated file_stats.py in probability_refreshment directory

--- probability_refreshment/file_stats.py
+++ probability_refreshment/file_stats.py
@@ -2,8 +2,7 @@ def file_stats( pathname ):
     all_items = []
     sqrt = lambda x : x ** 0.5
     with open( pathname, "r") as f:
-            all_items = ([(float(line)) for line in f])
-            
+            all_items = ([float(line) for line in f])
     avg = sum(all_items) / len(all_items)    
     std = sqrt(sum([(item - avg)**2 for item in all_items]) / (len(all_items) - 1) )
     return avg, std
